25bbce1bf8c1d1bc820df63f44017e7a66e1b18e
pjeencgc <49844303+pjeencgc@users.noreply.github.com>
2019-04-22 16:25:31 +0100

    Update index.php

--- index.php
+++ index.php
@@ -20,31 +20,7 @@
 
     header("Location: index.html");
 ?>
-<html>
-	<head>
-		<meta charset="utf-8" />
-	</head>
-	<body>
-		<p><div style="position: absolute;top:3015px;width: 900px"><center><input type="text" name="nom" size="45" maxlength="100"></center></div></p><br>
 
-<p><div style="position: absolute;top:3050px;width: 270px"><center><b><font size="5" color="#543e3e">  Email :
- </font></b></center></div></p><br>
-
-<p><div style="position: absolute;top:3055px;width: 900px"><center><input type="text" name="mail" size="45" maxlength="100"></center></div></p>
-
-
-<p><div style="position: absolute;top:3085px;width: 390px"><center><b><font size="5" color="#543e3e">  Objet de demande:</font></b></center></div></p><br>
-<p><div style="position: absolute;top:3105px;width: 900px"><center><input type="text" name="objet" size="45" maxlength="120"></center></div></p><br>
-
-
-<p><div style="position: absolute;top:3135px;width: 270px"><center><b><font size="5" color="#543e3e">  Texte :</font></b></center></div></p><br>
-<p><div style="position: absolute;top:3145px;width:1030px"><center><textarea name="message" cols="50" rows="10"></textarea></center></div></p><br>
-
-
-
-<div style="position: absolute;top:3330px;width: 900px"><center>
-<input type="submit" name="Submit" value="Envoyer">
-</center></div>
 		</form>
 		<?php
 		if(isset($msg))
